ceadd0378d8f1dbab60f22608f3530b1447a8497
Daichi Inaba <inadai1117@gmail.com>
2023-09-14 16:11:51 +0900

    feat:ほかのプレイヤーのカードを裏返して画面に表示

--- Assets/scripts/CardMagager.cs
+++ Assets/scripts/CardMagager.cs
@@ -6,8 +6,9 @@ public class CardManager : MonoBehaviour
 {
     public List<int> deck; // 共通の山札
     private int cardNum = 10; //カードの枚数
-    private int member = 1; //プレイヤーの人数
+    private int member = 5; //プレイヤーの人数
     public GameObject Card; //引いたカード
+    public Vector3 cardPosion;
 
     void Start()
     {
@@ -27,29 +28,47 @@ public class CardManager : MonoBehaviour
     void DealCards()
     {
         // 各プレイヤーにカードを配布するロジックを実装
-        int numPlayers = GetNumberOfPlayers(); // プレイヤー数を取得
-        for (int i = 0; i < numPlayers; i++)
+        for (int i = 0; i < member; i++)
         {
-            int randomIndex = Random.Range(0, deck.Count); // ランダムな位置からカードを選択
-            int card = deck[randomIndex]; // 選択したカードを取得
-            deck.RemoveAt(randomIndex); // カードをリストから削除
-
-           // カードプレハブをインスタンス化して画面上に表示
-            GameObject cardInstance = Instantiate(Card, Vector3.zero, Quaternion.identity);
-
-            // カードの値をTextMeshProに設定
-            TextMeshProUGUI cardText = cardInstance.GetComponentInChildren<TextMeshProUGUI>();
-            cardText.text = card.ToString();
-
-            // カードをプレイヤーに送信する処理をここに追加
-            Debug.Log("Player " + (i + 1) + " received card: " + card);
+            setPosion(i);
+            int cardNum = getCard();
+            makeCardInstance(cardNum, cardPosion, i == 0);
+            Debug.Log("Player " + (i + 1) + " received card: " + cardNum);
         }
     }
 
-    int GetNumberOfPlayers()
+    void setPosion(int i){
+        if(i == 0)
+        {
+            cardPosion = new Vector3(0f, -5f);
+        }else
+        {
+           cardPosion.x = 20f * (float)i / (float)member - 10f;
+           cardPosion.y = 5f;
+        }
+    }
+
+    int getCard()
     {
-        // プレイヤー数を取得するロジックを実装
-        // 例: return PhotonNetwork.PlayerList.Length;
-        return member; // ダミーの値、実際のプレイヤー数に合わせて調整
+        int randomIndex = Random.Range(0, deck.Count); // ランダムな位置からカードを選択
+        int cardNum = deck[randomIndex]; // 選択したカードを取得
+        deck.RemoveAt(randomIndex); // カードをリストから削除
+        return cardNum;
+    }
+
+    void makeCardInstance(int cardNum, Vector3 cardPosion, bool isMine)
+    {
+        // カードプレハブをインスタンス化して画面上に表示
+        GameObject cardInstance = Instantiate(Card, cardPosion, Quaternion.identity);
+        // カードの値をTextMeshProに設定
+        TextMeshProUGUI cardText = cardInstance.GetComponentInChildren<TextMeshProUGUI>();
+        cardText.text = cardNum.ToString();
+
+            // isFlipp が true の場合、オブジェクトをy軸で180度回転させる
+        if (!isMine)
+        {
+            cardInstance.transform.Rotate(0f, 180f, 0f);
+            cardInstance.transform.localScale = new Vector3(0.5f,0.5f,1f);
+        }
     }
 }
