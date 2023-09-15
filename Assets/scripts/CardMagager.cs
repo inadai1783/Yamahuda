b9fe083788a4d851aafbcb80e1ad6c8e76f4b9d3
Daichi Inaba <inadai1117@gmail.com>
2023-09-15 23:10:49 +0900

    feat:ホストのみがスタートボタンを実行可能にしました

--- Assets/scripts/CardMagager.cs
+++ Assets/scripts/CardMagager.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Photon.Pun;
 
-public class CardManager : MonoBehaviour
+public class CardManager : MonoBehaviourPun
 {
     public List<int> deck; // 共通の山札
     private int cardNum = 10; //カードの枚数
     private int member = 5; //プレイヤーの人数
-    public GameObject Card; //引いたカード
+    public GameObject CardPrefab; //引いたカード
     public Vector3 cardPosion;
 
     void Start()
@@ -27,48 +28,49 @@ public class CardManager : MonoBehaviour
 
     void DealCards()
     {
-        // 各プレイヤーにカードを配布するロジックを実装
-        for (int i = 0; i < member; i++)
-        {
-            setPosion(i);
+            cardPosion = new Vector3(0f, 0f, 0f);
             int cardNum = getCard();
-            makeCardInstance(cardNum, cardPosion, i == 0);
-            Debug.Log("Player " + (i + 1) + " received card: " + cardNum);
-        }
+            makeCardInstance(cardNum, cardPosion, false);
+            Debug.Log("Player received card: " + cardNum);
     }
+    
+        //    cardPosion.x = 20f * (float)i / (float)member - 10f; その他のカードを並べるときの座標参考
+        //    cardPosion.y = 5f;
 
-    void setPosion(int i){
-        if(i == 0)
-        {
-            cardPosion = new Vector3(0f, -5f);
-        }else
-        {
-           cardPosion.x = 20f * (float)i / (float)member - 10f;
-           cardPosion.y = 5f;
-        }
-    }
 
     int getCard()
     {
         int randomIndex = Random.Range(0, deck.Count); // ランダムな位置からカードを選択
         int cardNum = deck[randomIndex]; // 選択したカードを取得
         deck.RemoveAt(randomIndex); // カードをリストから削除
+        //SendDeckData(); //山札の同期
         return cardNum;
     }
 
-    void makeCardInstance(int cardNum, Vector3 cardPosion, bool isMine)
+    void makeCardInstance(int cardNum, Vector3 cardPosion, bool isFlipp)
     {
         // カードプレハブをインスタンス化して画面上に表示
-        GameObject cardInstance = Instantiate(Card, cardPosion, Quaternion.identity);
+        GameObject cardInstance = PhotonNetwork.Instantiate("Card", cardPosion, Quaternion.identity);
         // カードの値をTextMeshProに設定
         TextMeshProUGUI cardText = cardInstance.GetComponentInChildren<TextMeshProUGUI>();
         cardText.text = cardNum.ToString();
 
             // isFlipp が true の場合、オブジェクトをy軸で180度回転させる
-        if (!isMine)
+        if (isFlipp)
         {
             cardInstance.transform.Rotate(0f, 180f, 0f);
             cardInstance.transform.localScale = new Vector3(0.5f,0.5f,1f);
         }
     }
+
+    // public void SendDeckData()
+    // {
+    //    photonView.RPC("SyncDeck", RpcTarget.Others, deck); 
+    // }
+
+    // [PunRPC]
+    // public void SyncDeck(List<int> newDeck)
+    // {
+    //     deck = newDeck;
+    // }
 }
--- Assets/scripts/LobbyScene.cs
+++ Assets/scripts/LobbyScene.cs
@@ -1,28 +1,37 @@
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using TMPro; // TextMeshProUGUIを使用するためのディレクティブ
+using TMPro;
 
-
-// MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
 public class LobbyScene : MonoBehaviourPunCallbacks
 {
     public TextMeshProUGUI playerCountText; // TextMeshProUGUIの参照
-    private void Start() {
+    public Button startButton; // スタートボタンの参照
+
+    private void Start()
+    {
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
         PhotonNetwork.ConnectUsingSettings();
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
-    public override void OnConnectedToMaster() {
+    public override void OnConnectedToMaster()
+    {
         // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
         PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
     }
 
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
-    public override void OnJoinedRoom() {
+    public override void OnJoinedRoom()
+    {
         UpdatePlayerCountText();
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            startButton.interactable = true;
+        }
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
