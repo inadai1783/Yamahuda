2f2233e17db5ec8e01c200c8f69b9cf0eb398687
Daichi Inaba <inadai1117@gmail.com>
2023-09-16 15:53:04 +0900

    feat:山札をクリックすることでカードを引く

--- Assets/scripts/CardMagager.cs
+++ Assets/scripts/CardMagager.cs
@@ -8,17 +8,20 @@ public class CardManager : MonoBehaviourPun
     private int myPlayerID; //自信のプレイヤーID
     private int cardNum = 10; //カードの枚数
     private int members; //プレイヤーの人数
+    private bool isDraw;
     public List<int> deck; // 共通の山札
     public GameObject CardPrefab; //引いたカード
-    public Vector3 cardPosion = new Vector3(0f,0f,0f);
-    public Dictionary<int, int> playerCards = new Dictionary<int, int>(); // プレイヤーIDとカード番号の対応
+    public Vector3 cardPosion;
+    public Dictionary<int, int> playerCards; // プレイヤーIDとカード番号の対応
 
     void Start()
     {
+        isDraw = false;
         myPlayerID = PhotonNetwork.LocalPlayer.ActorNumber;
         members = PhotonNetwork.CurrentRoom.PlayerCount;
+        cardPosion = new Vector3(0f,-7f,0f);
+        playerCards = new Dictionary<int, int>();
         InitializeDeck(); // 山札を初期化
-        photonView.RPC("DealCard", RpcTarget.MasterClient, myPlayerID); // マスタークライアントにカードを引いてもらう
     }
 
     void InitializeDeck() //山札の初期化
@@ -30,12 +33,22 @@ public class CardManager : MonoBehaviourPun
         }
     }
 
+    public void OnDeckClicked()
+    {
+        if(!isDraw)
+        {
+            photonView.RPC("DealCard", RpcTarget.MasterClient, myPlayerID); // マスタークライアントにカードを引いてもらう
+            isDraw = true;
+        }
+    }
+
     [PunRPC]
     void DealCard(int playerID)
     {
             int number = getCard(); //ランダムなインデックス獲得
             Debug.Log("+player" + playerID + " : " + number);
             photonView.RPC("SendCardInfo", RpcTarget.All, playerID, number);
+            Debug.Log("Size:" + playerCards.Count);
     }
 
     int getCard()
@@ -72,5 +85,34 @@ public class CardManager : MonoBehaviourPun
         {
             makeCardInstance(false);
         }
+        if(members == playerCards.Count)
+        {
+            ViewOtherCards();
+        }
+    }
+    void ViewOtherCards()
+    {
+        float space = 20f / members;
+        Vector3 posion = new Vector3(space, 4f, 0f); // カードの表示開始位置
+
+        foreach (var kvp in playerCards)
+        {
+            int playerID = kvp.Key;
+            int cardNumber = kvp.Value;
+
+            if (playerID != myPlayerID)
+            {
+                // カードプレハブをインスタンス化して画面上部に表示
+                GameObject cardInstance = Instantiate(CardPrefab, posion, Quaternion.identity);
+
+                // カードの値をTextMeshProに設定
+                TextMeshProUGUI cardText = cardInstance.GetComponentInChildren<TextMeshProUGUI>();
+                cardText.text = cardNumber.ToString();
+
+                // カードの位置を調整して横並びに表示
+                posion.x += space;
+            }
+        }
+        Debug.Log("OKKK");
     }
 }
